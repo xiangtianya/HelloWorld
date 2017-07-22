9df0bf0e6fa09e202896f59b49483a2d3098c5c3
ken <dejinkong@qq.com>
2017-07-22 13:23:03 +0800

    Hello World
    Hello World

---
+++ HelloWorldTests/HelloWorldTests.m
@@ -0,0 +1,39 @@
+//
+//  HelloWorldTests.m
+//  HelloWorldTests
+//
+//  Created by dejin on 2017/5/14.
+//  Copyright © 2017年 dejin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloWorldTests : XCTestCase
+
+@end
+
+@implementation HelloWorldTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
